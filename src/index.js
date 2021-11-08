5d46a5c05b4184e6cca2201441859ad6b9353d2f
Somdotta07 <jhilick.s@gmail.com>
2021-11-08 22:31:35 +0100

    Add features to the project
    Add html
    Add css
    Add js files

--- src/index.js
+++ src/index.js
@@ -1,13 +1,7 @@
-import _ from 'lodash';
+/* eslint-disable linebreak-style */
 import './style.css';
-function component() {
-    const element = document.createElement('div');
-  
-   // Lodash, now imported by this script
-    element.innerHTML = _.join(['Hello', 'webpack'], ' ');
-    element.classList.add('hello');
-  
-    return element;
-  }
-  
-  document.body.appendChild(component());
+import { scoreList, updateList } from './scorelist.js';
+
+const scoreBoard = document.getElementById('scoreboard');
+
+updateList(scoreList, scoreBoard);
---
+++ src/scorelist.js
@@ -0,0 +1,21 @@
+/* eslint-disable linebreak-style */
+const scoreList = [
+  { name: 'Eva', score: 85 },
+  { name: 'Ada', score: 92 },
+  { name: 'Jim', score: 100 },
+  { name: 'Ana', score: 75 },
+  { name: 'Rob', score: 79 },
+];
+
+const updateList = (list, listContainer) => {
+  let scoreBoardContainer = '';
+  list.forEach((element) => {
+    const { name, score } = element;
+    scoreBoardContainer += `<li>
+      <p>${name} :${score}</p>
+      </li>`;
+  });
+  listContainer.innerHTML = scoreBoardContainer;
+};
+
+export { scoreList, updateList };
